73b87573362a527baf39b8fb4620a34662e27881
kuangshupei <zcapsku@gmail.com>
2024-09-18 22:52:35 +0100

    Results from Imperial HPC

---
+++ 800_UVC_heatmap_v2/.ipynb_checkpoints/run_800_neon-checkpoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=32:mem=128gb
+#PBS -l walltime=70:00:00
+#PBS -N run_800_neon
+#PBS -j oe
+#PBS -o /rds/general/user/sk1021/home/800_UVC_heatmap_v2/run_800_neon.log
+
+# load the necessary modules
+module load anaconda3/personal
+module load cuda/11.4.2
+
+# activate the conda environment
+eval "$(~/anaconda3/bin/conda shell.bash hook)"
+source activate simulation
+
+# Set the environment variable to disable file validation
+export PYDEVD_DISABLE_FILE_VALIDATION=1
+
+# navigate to the directory containing the notebook
+cd /rds/general/user/sk1021/home/800_UVC_heatmap_v2
+
+# run notebook
+python -Xfrozen_modules=off -m jupyter nbconvert --to notebook --execute 800_neon.ipynb --output 800_neon_executed.ipynb
+
+# deactivate the conda environment
+conda deactivate
